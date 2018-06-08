13a15fea1e343f3c864f484eabe932a85770994d
huqing <huqing@finchina.com>
2018-06-08 17:18:14 +0800

    testing file for Tools's GetExeDir

---
+++ Tools_test.go
@@ -0,0 +1,15 @@
+/**
+ * Created by huQg on 2018/6/8,008.
+ */
+
+package Tools
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestXYZ(t *testing.T) {
+	str := GetExeDir()
+	fmt.Printf("path = %s\n", str)
+}
